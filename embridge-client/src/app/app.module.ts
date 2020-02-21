39f87d61c4757a77da4ce4b4346b4d4df606a031
claranceatgalvanize <claranceatgalvanize@gmail.com>
2020-02-21 03:10:33 -0700

    added base components

--- embridge-client/src/app/app.module.ts
+++ embridge-client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { JobComponent } from './job/job.component';
+import { JoblistComponent } from './joblist/joblist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    JobComponent,
+    JoblistComponent
   ],
   imports: [
     BrowserModule,
---
+++ embridge-client/src/app/joblist/joblist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-joblist',
+  templateUrl: './joblist.component.html',
+  styleUrls: ['./joblist.component.scss']
+})
+export class JoblistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
