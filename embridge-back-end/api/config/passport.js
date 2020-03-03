3d9a8c9b94fac1881de19cc05095f7b7219c9f68
claranceatgalvanize <claranceatgalvanize@gmail.com>
2020-03-02 20:54:49 -0700

    schema and passport set up completed

--- embridge-back-end/api/config/passport.js
+++ embridge-back-end/api/config/passport.js
@@ -3,3 +3,20 @@ const passport = require("passport"),
   mongoose = require("mongoose");
 
 const User = mongoose.model("User");
+
+passport.use(
+  new Strategy(
+    {
+      usernameField: "email"
+    },
+    (username, password, done) => {
+      User.findOne({ email, username }, (err, user) => {
+        if (err) return done(err);
+        if (!user) return done(null, false, { message: "User not found" });
+        if (!user.validPassword(password))
+          return done(null, false, { message: "Incorrect password" });
+        return done(null, user);
+      });
+    }
+  )
+);
